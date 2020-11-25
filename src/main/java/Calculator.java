028fee909864d6baea4d35474542e290a16e6060
STEPSTONE-ASA\gressp01 <PeGr>
2020-11-25 20:09:48 +0100

    Refactoring and check for numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,7 @@ import org.apache.commons.lang.math.NumberUtils;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
 
 public class Calculator {
     static String val1;
@@ -11,16 +11,34 @@ public class Calculator {
     static DecimalFormat df = new DecimalFormat("#.##");
     static Scanner scanner = new Scanner(System.in);
 
-    static void division(String val1, String val2){
-        double val1double = NumberUtils.toDouble(val1);
-        double val2double = NumberUtils.toDouble(val2);
 
-        if (val2double != 0){
+
+    private static Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
+    }
+
+
+
+    static void division(String val1, String val2){
+        val1 = val1.replace(",", ".");
+        val2 = val2.replace(",", ".");
+
+        if(isNumeric(val1) && isNumeric(val2)) {
+            double val1double = NumberUtils.toDouble(val1);
+            double val2double = NumberUtils.toDouble(val2);
+
             System.out.println("Full Result is: " + val1double / val2double);
             System.out.println("Rounded Result is: " + df.format(val1double / val2double));
 
-        } else {
+        } else if("0".equals(val2)) {
             System.out.println("0 not allowed for Division");
+        } else {
+            System.out.println("No numbers found");
         }
     }
 
